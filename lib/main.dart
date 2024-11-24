456169dada237f94f2e0961d07640284ffa7ef4a
Furkan Arslan <74710096+FurkanArslanF@users.noreply.github.com>
2024-11-24 15:24:02 +0300

    localization and calendar added
    dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,22 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:rencber_mobile/core/router/go_router.dart';
 import 'package:rencber_mobile/core/theme/theme.dart';
 import 'package:sizer/sizer.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  EasyLocalization.logger.enableBuildModes = [];
+  await EasyLocalization.ensureInitialized();
+  runApp(
+    EasyLocalization(
+      supportedLocales: const [Locale('tr'), Locale('en')],
+      path: 'assets/lang',
+      fallbackLocale: const Locale('tr'),
+      child: const ProviderScope(child: MyApp()),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -17,11 +29,11 @@ class MyApp extends StatelessWidget {
       child: Sizer(
         builder: (BuildContext context, Orientation orientation, ScreenType screenType) {
           return MaterialApp.router(
-            title: 'Oteller.com',
+            title: 'Rencber',
             debugShowCheckedModeBanner: false,
-            // localizationsDelegates: context.localizationDelegates,
-            // supportedLocales: context.supportedLocales,
-            // locale: context.locale,
+            localizationsDelegates: context.localizationDelegates,
+            supportedLocales: context.supportedLocales,
+            locale: context.locale,
             routerConfig: RouterManager.instance.router,
             theme: appTheme(context),
             showPerformanceOverlay: false,
