e03f3ec2d430ced161e268d6634eb97baeda58fb
Furkan Arslan <74710096+FurkanArslanF@users.noreply.github.com>
2024-11-27 13:42:22 +0300

    loading widget added

---
+++ lib/core/widget/loading/loading.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void appLoading(BuildContext context, bool isLoading) {
+  if (isLoading) {
+    showDialog(
+      context: context,
+      barrierColor: Colors.white.withOpacity(0.5),
+      barrierDismissible: false,
+      builder: (BuildContext dialogContext) {
+        return const Center(
+          child: CircularProgressIndicator.adaptive(),
+        );
+      },
+    );
+  }
+  if (!isLoading) {
+    Navigator.of(context, rootNavigator: true).pop();
+  }
+}
